7ca0588736c47480222a841b9607f289e5dc50f5
IngAl3j4ndr0 <alexnoel.10300437@gmail.com>
2020-04-15 14:50:46 -0500

    ASP NET Core developer exception page. Excepción en UseDeveloperExceptionPage()

--- EmployeeManagement/Startup.cs
+++ EmployeeManagement/Startup.cs
@@ -31,30 +31,21 @@ namespace EmployeeManagement
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            //ANHG:  If environment is Development then...
             if (env.IsDevelopment())
             {
                 #region First middleware
-                app.UseDeveloperExceptionPage(); 
+                app.UseDeveloperExceptionPage();
                 #endregion
             }
-
-            /*
-             * UseFileServer combines the functionality of UseStaticFiles, UseDefaultFiles and UseDirectoryBrowser middleware. DirectoryBrowser middleware, enables directory browsing and allows users to see files within a specified directory. We could replace UseStaticFiles and UseDefaultFiles middlewares with UseFileServer Middleware.
-             */
-
-            // Use UseFileServer instead of UseDefaultFiles & UseStaticFiles
-            FileServerOptions fileServerOptions = new FileServerOptions();
-            fileServerOptions.DefaultFilesOptions.DefaultFileNames.Clear();
-            fileServerOptions.DefaultFilesOptions.DefaultFileNames.Add("foo.html");
-            #region File server Middleware
-            app.UseFileServer(fileServerOptions); 
-            #endregion
+            app.UseFileServer(); 
 
             app.Run(async (context) =>
             {
+                throw new Exception("Esta es una excepcion mamalona");
                 await context.Response.WriteAsync("Hola mundo");
             });
+
+            
         }
     }
 }
